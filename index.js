65165ffaec402b8ae1b4e79fe07e0ab85bfe719a
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-18 13:55:00 -0400

    view tables

--- index.js
+++ index.js
@@ -15,19 +15,37 @@ function init() {
         .prompt({
             name: 'add',
             type: 'list',
-            message: 'What would you like to add?',
-            choices: ['department', 'role', 'employee', 'EXIT'],
+            message: 'Add or view?',
+            choices: ['add department', 'add role', 'add employee', 'view departments', 'view roles', 'view employees', 'EXIT'],
         })
         .then((responce) => {
             switch (responce.add) {
-                case 'department':
+                case 'add department':
                     dep()
                     break;
-                case 'role':
+                case 'add role':
                     role()
                     break;
-                case 'employee':
+                case 'add employees':
                     empl()
+                    break;
+                case 'view departments':
+                    connection.query(`SELECT * FROM department`, (error, row) => {
+                        if (error) throw error;
+                    console.log(row)});
+                    init()
+                    break;
+                case 'view roles':
+                    connection.query(`SELECT * FROM role`, (error, row) => {
+                        if (error) throw error;
+                    console.log(row)});
+                    init()
+                    break;
+                case 'view employee':
+                    connection.query(`SELECT * FROM employee`,(error, row) => {
+                        if (error) throw error;
+                    console.log(row)});
+                    init()
                     break;
                 case 'EXIT':
                     connection.end()
@@ -59,75 +77,109 @@ function dep() {
 }
 
 function role() {
-    inquirer
-        .prompt([{
-            name: 'department',
-            type: 'input',
-            message: 'In which department are you adding a role?'
-        },
-        {
-            name: 'name',
-            type: 'input',
-            message: 'What role would you like to add?'
-        },
-        {
-            name: 'salary',
-            type: 'input',
-            message: 'what is the salary of this role?'
-        }])
-        .then(responce => {
-            const depart = responce.department;
-            connection.query(
-                `INSERT INTO role SET ?`,
-                {
-                    title: responce.name,
-                    salary: responce.salary,
-                    department_id: `SELECT id FROM department WHERE name = ${depart}`
-                },
-                (error) => {
-                    if (error) throw error;
-                    console.log('Role sucessfully created!');
-                    connection.end();
-                    init();
+    connection.query(`SELECT * FROM department`, (res, error) => {
+        if (error) throw error;
+        inquirer
+            .prompt([{
+                name: 'department',
+                type: 'rawlist',
+                message: 'In which department are you adding a role?',
+                choices() {
+                    const arr = [];
+                    res.forEach(({ name }) => {
+                        arr.push(name);
+                    })
+                    return arr;
                 }
-            )
-        })
+            },
+            {
+                name: 'name',
+                type: 'input',
+                message: 'What role would you like to add?'
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'what is the salary of this role?'
+            }])
+            .then(responce => {
+                const department = responce.department;
+                connection.query('SELECT * FROM department', (pull, error) => {
+                    if (error) throw error;
+                    let filtered = pull.filter((pull) => {
+                        return pull.name == department;
+                    })
+                    let id = filtered[0].id;
+                    connection.query(
+                        `INSERT INTO role SET ?`,
+                        {
+                            title: responce.name,
+                            salary: responce.salary,
+                            department_id: id
+                        },
+                        (error) => {
+                            if (error) throw error;
+                            console.log('Role sucessfully created!');
+                            connection.end();
+                            init();
+                        }
+                    )
+                })
+            })
+    })
 }
 
 function empl() {
-    inquirer
-        .prompt([{
-            name: 'role',
-            type: 'input',
-            message: "What is this employee's role?"
-        },
-        {
-            name: 'first',
-            type: 'input',
-            message: "What is this employee's first name?"
-        },
-        {
-            name: 'last',
-            type: 'input',
-            message: "What is this employee's last name?"
-        },
-        ])
-        .then(responce => {
-            const resRole = responce.role;
-            connection.query(
-                `INSERT INTO employee SET ?`,
-                {
-                    first_name: responce.first,
-                    last_name: responce.last,
-                    role_id: `SELECT id FROM role WHERE title = ${resRole}`
-                },
-                (error) => {
-                    if (error) throw error;
-                    console.log('Employee sucessfully created!');
-                    connection.end();
-                    init();
+    connection.query(`SELECT * FROM role`, (res, error) => {
+        if (error) throw error;
+        inquirer
+            .prompt([{
+                name: 'role',
+                type: 'rawlist',
+                message: "What is this employee's role?",
+                choices() {
+                    const arr = [];
+                    res.forEach(({ title }) => {
+                        arr.push(title);
+                    })
+                    return arr;
                 }
-            )
-        })
+            },
+            {
+                name: 'first',
+                type: 'input',
+                message: "What is this employee's first name?"
+            },
+            {
+                name: 'last',
+                type: 'input',
+                message: "What is this employee's last name?"
+            },
+            ])
+            .then(responce => {
+                const role = responce.role;
+                connection.query('SELECT * FROM role', (pull, error) => {
+                    if (error) throw error;
+                    let filtered = pull.filter((pull) => {
+                        return pull.title == role;
+                    })
+                    let id = filtered[0].id;
+                    connection.query(
+                        `INSERT INTO employee SET ?`,
+                        {
+                            first_name: responce.first,
+                            last_name: responce.last,
+                            role_id: id
+                        },
+                        (error) => {
+                            if (error) throw error;
+                            console.log('Employee sucessfully created!');
+                            connection.end();
+                            init();
+                        }
+                    )
+                })
+            })
+    })
 }
 init();
