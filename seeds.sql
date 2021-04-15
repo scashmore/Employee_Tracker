63392a79e80667b404d3b48020ed8fa4b233bb66
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-15 13:16:08 -0400

    new seeds

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO department(id, name)
+VALUES (1, 'Human Resources');
+INSERT INTO department(id, name)
+VALUES (2, 'Production');
+INSERT INTO department(id, name)
+VALUES (3, 'Finance');
+
+INSERT INTO role(id, title, salary, department_id)
+VALUES (1, 'Manager', 70000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (2, 'Accountant', 40000, 3);
+
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (1, 'Susan', 'Ashmore', 1, null);
